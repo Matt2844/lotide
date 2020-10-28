b5dec4ec697b905477d73ca2315ad3456d9a14a3
Matt <matt.hd.ford@gmail.com>
2020-10-28 22:37:54 +0000

    Added countLetters.js lotide problem

---
+++ countLetters.js
@@ -0,0 +1,29 @@
+// Assert Equal function from previous exercise. 
+const assertEqual = function (actual, expected) {
+  if (actual !== expected) {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  } else if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  }
+};
+
+// New problem countLetters below
+// Automatically adds a letter key and updates its count. 
+const countLetters = function (str) {
+  const arr = str.split('');
+  const resultObj = {};
+
+  for (let val of arr) {
+    if (resultObj[val]) {
+      resultObj[val]++
+    } else {
+      resultObj[val] = 1
+    }
+  }
+  console.log(resultObj);
+  return resultObj
+}
+
+
+
+console.log(countLetters('LHL'))
