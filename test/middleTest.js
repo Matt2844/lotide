aee7dc0fb3b60b629249c4097eeb6baa54a671b3
Matt <matt.hd.ford@gmail.com>
2020-11-02 19:59:33 +0000

    Added mocha and chai test to middleTest.js

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,10 +1,25 @@
 // middle.js
 
+//const middle = require('../middle');
+//const isArrEqual = require("../assertArraysEqual")
+
+const assert = require('chai').assert;
 const middle = require('../middle');
-const isArrEqual = require("../assertArraysEqual")
 
+describe("#middle", () => {
+  it("returns a middle number for an array with odd amount of items", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+  })
+  it("return two middle numbers if array is even", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3])
+  })
+  it("returns an empty array if there are only 2 items or less", () => {
+    assert.deepEqual(middle([1, 2]), []);
+  })
+})
 
-//Test cases: 
+/*
+//Test cases:
 console.log(isArrEqual(middle([1]))) // => []
 console.log(middle([1, 2])) // => []
 
@@ -13,3 +28,4 @@ console.log(middle([1, 2, 3, 4, 5])) // => [3]
 
 console.log(isArrEqual(middle([1, 2, 3, 4]))) // => [2, 3]
 console.log(middle([1, 2, 3, 4, 5, 6])) // => [3, 4]
+*/
