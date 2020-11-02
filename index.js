058e507429bcf0ed3253eab80ff5f2986a5332f8
Matt <matt.hd.ford@gmail.com>
2020-11-02 21:44:17 +0000

    index.js object lotideLibrary  shorthand edit

---
+++ index.js
@@ -0,0 +1,39 @@
+
+const head = require('./head');
+const tail = require('./tail');
+const middle = require('./middle');
+const isArrEqual = require('./assertArraysEqual');
+const assertEqual = require('./assertEqual');
+const countLetters = require('./countLetters');
+const countOnly = require('./countOnly');
+const eqArrays = require('./eqArrays');
+const eqObjects = require('./eqArrays');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+const letterPositions = require('./letterPositions');
+const map = require('./map');
+const takeUntil = require('./takeUntil');
+const without = require('./without');
+
+module.exports = lotideLibrary
+
+
+const lotideLibrary = {
+  head: head,
+  tail: tail,
+  middle: middle,
+  isArrEqual: isArrEqual,
+  assertEqual: assertEqual,
+  countLetters: countLetters,
+  countOnly: countOnly,
+  eqArrays: eqArrays,
+  eqObjects: eqObjects,
+  findKey: findKey,
+  findKeyByValue: findKeyByValue,
+  letterPositions: letterPositions,
+  map: map,
+  takeUntil: takeUntil,
+  without: without,
+}
+
+// Come back to edit findKeyByValue if there is time. 
